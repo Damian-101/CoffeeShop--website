b9acf3d438601f8e3dbe578d0c80fa614bb4bf09
Shenal <shenal6x@gmail.com>
2021-04-01 00:39:31 +0530

    conten added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// nav scroll fixed position 
 window.addEventListener("scroll",scrollFixed)
 function scrollFixed () {
        const nav = document.getElementsByClassName("nav")[0]
@@ -10,63 +11,84 @@ function scrollFixed () {
 }
 scrollFixed()
 
-const swiperCarousel = () => {
-  //check overflow from an array of items
-  const carouselItemOverFlow = () => {
-    const carouselItems = document.getElementsByClassName("ourProducts__item")
-    const carouselNavigation = document.getElementById("carouselNavigation")
-    if(carouselItems.length > 4){
-      return true;
-    }else{
-      return false;
-    }
-  }
-  if(window.innerWidth > 1500 && carouselItemOverFlow() !== true){
-    carouselNavigation.classList.add("dis-none-1000")
-    const swiper = new Swiper('.swiper-container', {
-      centeredSlides: true,
-      centeredSlidesBounds: true,
-      allowTouchMove: false,
+       const swiper = new Swiper('.swiper-container', {
+              width: 1000,
+              slidesPerView: 1,
+              spaceBetween: 10,
+              breakpoints: {
+                     // when window width is >= 320px
+                     320: {
+                       slidesPerView: 2,
+                       spaceBetween: 20
+                     },
+                     // when window width is >= 480px
+                     480: {
+                       slidesPerView: 3,
+                       spaceBetween: 30
+                     },
+                     // when window width is >= 640px
+                     640: {
+                       slidesPerView: 2,
+                       spaceBetween: 80
+                     }
+                   },
+                 // Optional parameters
+                 direction: 'horizontal',
+                //  Navigation arrows
+                 navigation: {
+                   nextEl: '.swiper-button-next',
+                   prevEl: '.swiper-button-prev',
+                 },
      });
-  }else{
-    console.log(false)
-  }
-  const swiper = new Swiper('.swiper-container', {
-    centeredSlides: true,
-    centeredSlidesBounds: true,
-    // allowTouchMove: false,
-      breakpoints: {
-        500:{
-          slidesPerView: 1
-        },
-        790:{
-          slidesPerView: 2
-        },
-        1000:{
-          slidesPerView: 3,
-        },
-        1500:{
-          slidesPerView: 4,
-        }
-      },
-       // Optional parameters
-       direction: 'horizontal',
-      //  Navigation arrows
-      //  navigation: {
-      //    nextEl: '.swiper-button-next',
-      //    prevEl: '.swiper-button-prev',
-      //  },
-     });
+
+const prevBtn = document.getElementById("prev-btn") 
+const nextBtn = document.getElementById("next-btn") 
+function customCarousel () {
+       // item array 
+       const items = document.querySelectorAll(".ourProducts__item")
+       const itemRefImages = document.querySelectorAll(".ourProducts__item-ref-img")
+       // iterate nodes values 
+       items.forEach((currentValue, currentIndex) =>{
+              currentValue.classList.forEach((value) => {
+                     if(value === "swiper-slide-active"){
+                            itemRefImages[currentIndex].classList.add("ourProducts--img-ref-active")
+                            items[currentIndex].classList.add("item--active-size")
+                     }else if(itemRefImages[currentIndex] !== undefined){
+                            itemRefImages[currentIndex].classList.remove("ourProducts--img-ref-active")
+                            items[currentIndex].classList.remove("item--active-size")
+                            if(swiper.isEnd === true){
+                                   nextBtn.style.opacity = "50%"
+                                   window.nextDisable = true
+                            }
+                            else{
+                                   nextBtn.style.opacity = "100%"
+                                   window.nextDisable = false
+                            }
+                            if(swiper.isBeginning === true){
+                                   prevBtn.style.opacity = "50%"
+                                   window.prevDisable = true
+                            }
+                            else{
+                                   prevBtn.style.opacity = "100%"
+                                   window.prevDisable = false
+                            }
+                     }
+              })
+       })
+}
+customCarousel()
 
   // carousel custom buttons 
-  const prevBtn = document.getElementById("prev-btn") 
   prevBtn.addEventListener("click", () => {
-    swiper.slidePrev()
+       if(window.prevDisable === false){
+              swiper.slidePrev()
+              customCarousel()
+       }
   })
-  const nextBtn = document.getElementById("next-btn") 
   nextBtn.addEventListener("click", () => {
-    swiper.slideNext()
+       if(window.nextDisable === false){
+              swiper.slideNext()
+              customCarousel()
+              console.log(window.nextDisable)
+       }
   })
-}
-
-swiperCarousel()
